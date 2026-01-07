b81e4f3149c50d724f02ce2dbdc23302cb1576a4
Alex <foulonalex@yahoo.com>
2026-01-07 14:04:23 +0100

    Small fix

--- src/layers/activationLayers.cpp
+++ src/layers/activationLayers.cpp
@@ -1,7 +1,6 @@
 #include "layers/activationLayers.hpp"
 #include <cmath>
 
-// ReLULayer: max(0, x)
 Eigen::VectorXf ReLULayer::forward(const Eigen::VectorXf& input, bool cacheEnabled)
 {
     cachedOutput = input.array().max(0.0f).matrix();
@@ -20,7 +19,6 @@ Eigen::VectorXf ReLULayer::backward(const Eigen::VectorXf& outputGradient, float
     return outputGradient.cwiseProduct(cachedDerivatives);
 }
 
-// LinearLayer: f(x) = x
 Eigen::VectorXf LinearLayer::forward(const Eigen::VectorXf& input, bool cacheEnabled)
 {
     cachedOutput = input;
@@ -39,10 +37,9 @@ Eigen::VectorXf LinearLayer::backward(const Eigen::VectorXf& outputGradient, flo
     return outputGradient;
 }
 
-// SoftmaxLayer: e^x_i / sum(e^x_j)
 Eigen::VectorXf SoftmaxLayer::forward(const Eigen::VectorXf& input, bool cacheEnabled)
 {
-    // Numerical stability: subtract max before exp
+    // Subtract max before exp for numerical stability
     float maxInput = input.maxCoeff();
     Eigen::VectorXf exps = (input.array() - maxInput).exp();
     float sumExps = exps.sum();
@@ -51,8 +48,7 @@ Eigen::VectorXf SoftmaxLayer::forward(const Eigen::VectorXf& input, bool cacheEn
     if (cacheEnabled)
     {
         cachedInput = input;
-        // For softmax: da_i/dz_j = softmax_i * (delta_ij - softmax_j)
-        // Store softmax values for backward pass
+        // Here we just cache the output for use in backwards pass because the full Jacobian is memory intensive
         cachedDerivatives = cachedOutput;
     }
 
@@ -61,18 +57,6 @@ Eigen::VectorXf SoftmaxLayer::forward(const Eigen::VectorXf& input, bool cacheEn
 
 Eigen::VectorXf SoftmaxLayer::backward(const Eigen::VectorXf& outputGradient, float learningRate)
 {
-    // For softmax, compute Jacobian-vector product
-    // da_i/dz_j = softmax_i * (delta_ij - softmax_j)
-    Eigen::VectorXf jacobianProduct = Eigen::VectorXf::Zero(cachedDerivatives.size());
-    
-    for (int i = 0; i < cachedDerivatives.size(); ++i)
-    {
-        for (int j = 0; j < cachedDerivatives.size(); ++j)
-        {
-            float jacobian = cachedDerivatives[i] * (i == j ? 1.0f : 0.0f) - cachedDerivatives[i] * cachedDerivatives[j];
-            jacobianProduct[i] += jacobian * outputGradient[j];
-        }
-    }
-    
-    return jacobianProduct;
+    float dotProduct = cachedDerivatives.dot(outputGradient);
+    return (cachedDerivatives.array() * (outputGradient.array() - dotProduct)).matrix();
 }
